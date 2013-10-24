41b57cfffd5166079f194ac55962a11551c8b43b
RobMoore902 <capital.r@gmail.com>
2013-10-24 15:34:56 -0300

    Added youtube searching

--- SoundCloud/js/main.js
+++ SoundCloud/js/main.js
@@ -3,6 +3,8 @@ var client_id = '1ff3601991d9fcdd7d55d737080fbe4a';
 var SEARCH_ARTIST_URL = 'https://api.soundcloud.com/tracks.json?client_id='+
 	client_id +'&q=';
 
+var SEARCH_YOUTUBE_URL = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=wutang&key=AIzaSyDqOskIfAmyH9bciyMTcbjGyLWqxHWaSjQ'
+
 
 //Search for an artist/band
 $("#search_button").click(function() {
@@ -16,5 +18,12 @@ $("#search_button").click(function() {
           	$("#results").append("<p>"+this.title+"</p>");
            })
          });
+		 
+		$.getJSON(SEARCH_ARTIST_URL + search_term, function(tracks) {
+          $(tracks).each(function(track) {
+          	$("#YTresults").append("<p>"+this.title+"</p>");
+           })
+         });
+		 
 
 });
