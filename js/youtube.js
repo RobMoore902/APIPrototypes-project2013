3cd6102cff6bf74072b568724497118a85a47413
RobMoore902 <capital.r@gmail.com>
2013-10-31 12:51:14 -0300

    Added youtube searching. Fixed top menu.

--- js/youtube.js
+++ js/youtube.js
@@ -1,4 +1,13 @@
 //Search for an artist/band
+
+var SEARCH_YOUTUBE_URL = 'https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyDqOskIfAmyH9bciyMTcbjGyLWqxHWaSjQ&q='
+
 function searchYoutube() {
-	console.log('Youtube not yet implemented');
+
+	var search_term = $("#search_input").val();
+	$.getJSON(SEARCH_YOUTUBE_URL + search_term, function(tracks) {
+          $(tracks.items).each(function(track) {
+          	$("#YTresults").append("<p>"+this.snippet.title+"</p>");
+           })
+         });
 }
